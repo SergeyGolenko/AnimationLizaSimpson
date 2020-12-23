56c0f68fa5fd4b249dc033aae07b903b9f22adbe
SergeyGolenko <1989sergiotay@gmail.com>
2020-12-23 12:32:51 +0200

    done

--- AnimationLizaSimpson/ViewController.swift
+++ AnimationLizaSimpson/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+ 
+    @IBOutlet weak var imageLiza: UIImageView!
+    var count = 1
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       reloudLiza()
+    }
+    
+    func reloudLiza(){
+        imageLiza.image = UIImage(named: "frame_0_delay-0.07s")
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.view.addGestureRecognizer(tap)
+        view.isUserInteractionEnabled = true
     }
 
 
+    
+
+    // function which is triggered when handleTap is called
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        imageLiza.image = UIImage(named: "frame_\(count)_delay-0.07s")
+        count += 1
+        if count == 3{
+            count = 0
+        }
+    }
+    
+    
+    
+    
+    
 }
 
